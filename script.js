a9d9692f022216d03da2b47cdd54e852a99e07eb
Paula Fenner <fenner.paula@gmail.com>
2024-06-28 19:05:22 +0200

    Added the delete button, and implemented some other functions

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 const inputBtnEl = document.getElementById('input-btn');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+const deleteBtnEl = document.getElementById('delete-btn');
 
 let myLeads = [];
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
 inputBtnEl.addEventListener('click', execute);
+deleteBtnEl.addEventListener('dblclick', clearAll);
 
 function saveLead() {
   myLeads.push(inputEl.value);
 }
 
-function renderLeads() {
+function storingLocal() {
+  // Save the myLeads array to localStorage
+  // PS: remember JSON.stringify()
+  localStorage.setItem('myLeads', JSON.stringify(myLeads));
+}
+
+function render(leads) {
   let listItems = '';
-  for (const lead of myLeads) {
+  for (const lead of leads) {
     listItems += `
     <li>
         <a 
@@ -27,12 +41,19 @@ function renderLeads() {
   ulEl.innerHTML = listItems;
 }
 
-function clearAll() {
+function clearInput() {
   inputEl.value = '';
+}
+
+function clearAll() {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
 }
 
 function execute() {
   saveLead();
-  clearAll();
-  renderLeads();
+  clearInput();
+  render(myLeads);
+  storingLocal();
 }
